72908c88897e0efc096aaaf7d8f187095292f0d1
PearsonGrant24 <100286838+PearsonGrant24@users.noreply.github.com>
2025-07-06 20:59:37 +0200

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "pear-demo1212221" # Replace with your actual S3 bucket name
+    bucket = "gpt-demo1212221" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
